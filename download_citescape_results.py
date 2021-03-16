05acfe680996bf410284614e626d35ba44c5a085
udithhaputhanthri <170208k@uom.lk>
2021-03-16 17:23:45 +0530

    added ALAE citescapes results | mode collapsing was happened after ~900 epochs

--- download_citescape_results.py
+++ download_citescape_results.py
@@ -6,7 +6,7 @@ os.mkdir('citescapes_results_images')
 
 
 for file_ in os.listdir('training_artifacts/citescapes'):
-    if ('.jpg' in file_ or '.png' in file_) and '_4' in file_:
+    if ('.jpg' in file_ or '.png' in file_) and '_26' in file_:
         shutil.copy(f'training_artifacts/citescapes/{file_}',f'citescapes_results_images/{file_}')
 
 shutil.make_archive('citescapes_results_download','zip','citescapes_results_images')
